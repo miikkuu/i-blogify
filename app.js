ad49410e064b604ac61abe32757180b48567666e
miikkuu <itsmeyashraj.official@gmail.com>
2024-06-06 16:15:34 +0530

    error solved

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const config = require("./config/config");
+
+const PORT = config.app.port;
+
+
+
+
 require("./config/db");
 const path = require("path");
 const express = require("express");
@@ -19,19 +26,35 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
-app.use(express.static(path.resolve("./public")));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.get("/", async (req, res) => {
-  const allBlogs = await Blogs.find({});
-  res.render("home", {
-    user: req.user,
-    blogs: allBlogs,
-  });
+  try {
+    const allBlogs = await Blogs.find({});
+    res.render("home", {
+      user: req.user,
+      blogs: allBlogs,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('An error occurred');
+  }
 });
 app.use("/user", userRoutes);
 app.use("/blog", blogRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
